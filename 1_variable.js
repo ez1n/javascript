3fc2c6a6c0d925d23019f0037844674677922753
unknown <eodrmfdl1004@naver.com>
2023-02-24 16:44:30 +0900

    변수

---
+++ 1_variable.js
@@ -0,0 +1,17 @@
+/** 변수 */
+
+// 변수 선언 + 값 할당
+let a = 0;
+console.log(a); // 0
+
+// 재할당
+a = 1;
+console.log(a); // 1
+
+//변수 선언
+let b;
+console.log(b); // undefined
+
+// 값 할당
+b = 2;
+console.log(b); // 2
