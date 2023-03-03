b7ab6997e111751464bbcaf11ba5bdcdaad67475
unknown <eodrmfdl1004@naver.com>
2023-03-03 15:35:41 +0900

    객체 타입

---
+++ 7_object.js
@@ -0,0 +1,26 @@
+/** 객체 타입 */
+
+// Heap(메모리)에 저장됨
+
+let name = "apple";
+let color = "red";
+let display = "🍎";
+
+let apple = {
+  name: "사과",
+  color: "red",
+  display: "🍎",
+};
+
+console.log(apple); // { name: '사과', color: 'red', display: '🍎' }
+console.log(apple.name); // 사과
+console.log(apple.color); // red
+console.log(apple.display); // 🍎
+
+let orange = {
+  name: "오렌지",
+  color: "orange",
+  display: "🍊",
+};
+
+console.log(orange); //{  name: 'orange', color: 'orange', display: '🍊' }
