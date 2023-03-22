f545f793ea7f65965c828edc3b21478183443b9d
unknown <eodrmfdl1004@naver.com>
2023-03-22 17:56:28 +0900

    상속 퀴즈

---
+++ quiz2.js
@@ -0,0 +1,40 @@
+/** 퀴즈 */
+
+// 정직원과 파트타임직원을 나타낼 수 있는 클래스를 만들어 보자.
+// 직원들의 정보: 이름, 부서이름, 한달 근무 시간
+// 매달 직원들의 정보를 이용해서 한달 월급을 계산할 수 있다.
+// 정직원은 시간 당 10000원
+// 파트타임 직원은 시간 당 8000원
+
+class Employee {
+  constructor(name, department, hoursPerMonth, payRate) {
+    this.name = name;
+    this.department = department;
+    this.hoursPerMonth = hoursPerMonth;
+    this.payRate = payRate;
+  }
+
+  calculatePay() {
+    return this.hoursPerMonth * this.payRate;
+  }
+}
+
+class FullTimeEmployee extends Employee {
+  static #PAY_RATE = 10000;
+
+  constructor(name, department, hoursPerMonth) {
+    super(name, department, hoursPerMonth, FullTimeEmployee.#PAY_RATE);
+  }
+}
+
+class PartTimeEmployee extends Employee {
+  static #PAY_RATE = 8000;
+  constructor(name, department, hoursPerMonth) {
+    super(name, department, hoursPerMonth, PartTimeEmployee.#PAY_RATE);
+  }
+}
+
+const YJ = new FullTimeEmployee("예진", "s/w", 30);
+const Bob = new PartTimeEmployee("Bob", "s/w", 20);
+console.log(YJ.calculatePay());
+console.log(Bob.calculatePay());
