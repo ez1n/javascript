3e0ba9f76829c0aa62a2b9b059a7e7a6b8d2668e
unknown <eodrmfdl1004@naver.com>
2023-05-04 14:55:07 +0900

    불리언 함수

---
+++ 3_boolean.js
@@ -0,0 +1,24 @@
+/** 불리언 함수 (Boolean) */
+
+const isTrue = true; // new Boolean(true);
+console.log(isTrue.valueOf()); // true
+
+/**
+ * Falshy
+ * 0
+ * -0
+ * null
+ * NaN
+ * undefined
+ * ''
+ */
+
+/**
+ * Truthy
+ * 1
+ * -1
+ * '0'
+ * 'false'
+ * []
+ * {}
+ */
