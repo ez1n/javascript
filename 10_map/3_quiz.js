48023ab0193e991db933e770e6b4ffda57a7eb9e
ez1n <eodrmfdl1004@naver.com>
2024-01-15 15:11:48 +0900

    퀴즈

---
+++ 10_map/3_quiz.js
@@ -0,0 +1,13 @@
+// 주어진 배열에서 중복을 제거 하라
+const fruits = ['🍌', '🍎', '🍇', '🍌', '🍎', '🍑'];
+//  ['🍌', '🍎', '🍇', '🍑']
+const result = new Set(fruits);
+console.log([...result]);
+
+
+// 주어진 두 세트의 공통된 아이템만 담고 있는 세트 만들어라
+const set1 = new Set([1, 2, 3, 4, 5]);
+const set2 = new Set([1, 2, 3]);
+const result2 = new Set();
+set1.forEach(value => set2.has(value) && result2.add(value));
+console.log(result2);
